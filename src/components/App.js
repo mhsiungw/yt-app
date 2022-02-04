74c77337d6bd237492acc94b51b481e66749dde4
minhsiungw <s16103145@gmail.com>
2022-02-04 09:54:30 +0800

    fix: App not shown on Safari

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
             <div>
                 <BrowserRouter>
                     <Routes>
-                        <Route exact path="/" element={<Videos />} />
+                        <Route path="/" element={<Videos />} />
                         <Route path="*" element={<NotFound />} />
                     </Routes>
                 </BrowserRouter>
